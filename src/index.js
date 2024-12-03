d995cbae90dd540532e358813fc4bc223f415e8c
Auxtine <augustineezra@gmail.com>
2024-12-03 13:30:36 +0100

    Remove eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
-  </React.StrictMode>,
+    </HashRouter>
+  </React.StrictMode>
 );
